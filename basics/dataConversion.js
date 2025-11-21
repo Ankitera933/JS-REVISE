4b1d5815d3c254648d4833b84946e561b545c819
“Ankitkumar <ankitkumarthat182@gmail.com>
2025-11-21 19:19:46 +0530

    first part

--- basics/dataConversion.js
+++ basics/dataConversion.js
@@ -4,3 +4,17 @@ console.log(typeof(score))
 let intNumber=Number(score);
 console.log(typeof(intNumber))
 console.log(intNumber)
+
+//33=>33
+//"33absc"=>NaN
+//true=>1
+//false=>0
+
+
+let isLoggeI=1;
+let booleanIsLoggedIn=Boolean(isLoggeI)
+console.log(booleanIsLoggedIn)
+
+// 1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
