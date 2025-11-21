5485bc0f6fc9fae3db261d766e8667baa237f03c
“Ankitkumar <ankitkumarthat182@gmail.com>
2025-11-21 16:53:47 +0530

    Add basics: dataConversion.js and datatypes.js

---
+++ basics/dataConversion.js
@@ -0,0 +1,6 @@
+let score="22aa"
+
+console.log(typeof(score))
+let intNumber=Number(score);
+console.log(typeof(intNumber))
+console.log(intNumber)
---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"// treat all code as newer version of code
+
+//alert ("Be ready") // using node js not browser
+
+console.log(2+3);
+console.log("Ankit")
+let name="ANkit"
+let age=14
+let isLoggedIn=false;
+// number ->2 to power 53 
+//bigint
+//string
+//boolean
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof  undefined)//undefined
+console.log(typeof null)// object
+
+
