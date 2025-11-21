dabdff60fd623944938f4f6ebf451d20cb3a6c19
“Ankitkumar <ankitkumarthat182@gmail.com>
2025-11-21 19:47:14 +0530

    final Part

--- basics/dataConversion.js
+++ basics/dataConversion.js
@@ -18,3 +18,23 @@ console.log(booleanIsLoggedIn)
 // 1=>true; 0=>false
 //""=>false
 //"hitesh"=>true
+
+// ********** Operations **********
+ let value=3
+ let negValue=-value
+ console.log(negValue)
+
+ console.log(2/3)
+ console.log(2+"3")//23
+ console.log(2+"hi")//2hi
+ console.log("2"+3)//23
+ console.log(1+2+"3")//33
+ console.log(+true)//1
+
+ //*************************Increment************* *//
+ let x=3;
+ let y=x++;
+ console.table([x,y])// 4,3
+ let a=3;
+ let b=++a;
+  console.table([a,b])//4,4
