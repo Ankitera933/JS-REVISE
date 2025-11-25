53082f4a7d6eda0ec10455cdc00d4e890f44d19b
“Ankitkumar <ankitkumarthat182@gmail.com>
2025-11-25 22:46:50 +0530

    done

---
+++ basics/Strings.js
@@ -0,0 +1,17 @@
+const name="ANkit"
+const count=50
+
+console.log(`My name is ${name} and I have ${count} repo`)
+console.log(name[0].toUpperCase())
+console.log(name.length)
+const example="           Hello people of india, I am ur president from USA and Just i dont care about u and ur problem so bye and shove it to ur ass    "
+console.log(example.replace("india","USA"))
+console.log(example.split(","))
+console.log(example.includes("president"))
+console.log(example.split(" "))
+console.log(example.trim())
+console.log(example.trim().substring(0,10))
+console.log(example.indexOf("p"))
+
+const Onename=new String("ANkit works here")
+console.log(Onename[0])
